72ed858cbd560272c2dd85317bad7cdc62ea731c
richiekehnet <richiekehnet@gmail.com>
2025-08-26 02:18:56 -0600

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: 'https://oprahomes.ca',   // your custom domain
   generateRobotsTxt: true,           // automatically create robots.txt
-  sitemapSize: 7000,                 // optional, split if large
+  sitemapSize: 5000,                 // optional, split if large
   changefreq: 'daily',               // optional, update frequency
   priority: 0.7                      // optional, default priority
 };
