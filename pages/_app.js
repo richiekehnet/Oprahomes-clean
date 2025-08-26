a57743e6c0530b7fd86458276a49d4c2f2269b2c
richiekehnet <richiekehnet@gmail.com>
2025-08-26 02:23:15 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,5 @@
-import Head from 'next/head'
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "RealEstateAgent",
-              "name": "Oprahomes",
-              "url": "https://oprahomes.ca",
-              "logo": "https://oprahomes.ca/logo.png",
-              "sameAs": [
-                "https://www.instagram.com/oprahomes",
-                "https://www.youtube.com/@oprahomes"
-              ],
-              "address": {
-                "@type": "PostalAddress",
-                "addressLocality": "Calgary",
-                "addressRegion": "AB",
-                "postalCode": "T3K",
-                "addressCountry": "CA"
-              }
-            })
-          }}
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
