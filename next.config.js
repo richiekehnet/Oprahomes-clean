be6ba1842098008f44d958a8719671975501aab2
richiekehnet <richiekehnet@gmail.com>
2025-09-05 20:59:24 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export', // <-- this replaces `next export`
+}
 
-module.exports = {
-  output: 'export',
-};
+module.exports = nextConfig
