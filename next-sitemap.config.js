30d9b89096ad7aa180e387be8f7b19f5175a229a
richiekehnet <richiekehnet@gmail.com>
2025-09-05 20:52:56 -0600

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://oprahomes.ca',   // your custom domain
-  generateRobotsTxt: true,           // automatically create robots.txt
-  sitemapSize: 5000,                 // optional, split if large
-  changefreq: 'daily',               // optional, update frequency
-  priority: 0.7                      // optional, default priority
-};
